b77474ace806c46f2fe2f6b9722744898b4fb1b3
xoarty1002-eng <xoarty1002@gmail.com>
2026-08-14 04:26:17 +0300

    up

--- FairAI/Program.cs
+++ FairAI/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using FairAI;
+
+while (true)
+{
+    var request = Console.ReadLine();
+    ILanguage dm;
+    IDepth depth;
+    ICore core;
+    //up
+    IDepth depth2;
+    ILanguage dm2;
+    var response = "answer: "+ request;
+    Console.WriteLine(response);
+}
